626fe913f08f1c283dea8517fc1da12a23445bc7
Shiblon <spahulu@byu.edu>
2019-03-06 18:21:01 -0600

    added persistent variables through localStorage with CRUD ability that works alongside document

--- app.js
+++ app.js
@@ -7,12 +7,32 @@ const taskInput = document.querySelector('#task');
 loadEventListeners();
 
 function loadEventListeners(){
+    document.addEventListener('DOMContentLoaded', getTasks);
     form.addEventListener('submit', addTask);
     taskList.addEventListener('click', removeTask);
     clrBtn.addEventListener('click', clearTasks);
     filter.addEventListener('keyup', filterTasks);
 }
-
+//to make sure that the tasks that are displayed on local storage are displayed upon refresh
+function getTasks(){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.forEach(function(task){
+    //same code as dynamically adding the items 
+    const li = document.createElement('li');
+    li.className = 'collection-item';
+    li.appendChild(document.createTextNode(task));
+    const link = document.createElement('a');
+    link.className = 'delete-item secondary-content';
+    link.innerHTML = '<i class = "fa fa-remove"></i>';
+    li.appendChild(link);
+    taskList.appendChild(li);
+    });
+}
 function addTask(e){
     //for better ux - ensure that the user enters something for the task
     if(taskInput.value === ''){
@@ -32,17 +52,45 @@ function addTask(e){
     //appending the newly created li to the ul
     taskList.appendChild(li);
     //clearing the input in the textbox
+    storeTaskInLocalStorage(taskInput.value);
     taskInput.value = ''; 
 
     e.preventDefault();
 }
+function storeTaskInLocalStorage(task){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
 function removeTask(e){
     console.log(e.target.parentElement.parentElement.value);
     if (e.target.parentElement.classList.contains('delete-item')){
         if(confirm('Are you sure you want to delete?')){
             e.target.parentElement.parentElement.remove();
+
+            removeTaskFromLocalStorage(e.target.parentElement.parentElement);
         }
     }
+}
+function removeTaskFromLocalStorage(taskItem){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    }else{
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.forEach(function(task, index){
+        if(taskItem.textContent === task){
+            tasks.splice(index, 1);
+        }
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 function clearTasks(e){
     while(taskList.firstChild){
@@ -51,8 +99,10 @@ function clearTasks(e){
 }
 function filterTasks(e){
     const text = e.target.value.toLowerCase();
+    //can use the .forEach b/c of the querySelectorAll which returns a node list
     document.querySelectorAll('.collection-item').forEach(function(task){
         const item = task.firstChild.textContent;
+        //text is the parameter that is passed by the user to compare to list items collected by .firsChild in iterration forEach 
         if(item.toLowerCase().indexOf(text) != -1){
             task.style.display = 'block';
         }else {
